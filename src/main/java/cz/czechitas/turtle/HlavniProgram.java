c9cad5a33ca9bb0f72d933b9e7b901c5a8c54505
Natálie Drápelová <simonova.natla@gmail.com>
2024-02-11 12:06:43 +0100

    metody - část 1

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,8 +9,61 @@ public class HlavniProgram {
         new HlavniProgram().start();
     }
 
-    public void start() {
-        //TODO Tady bude kód pro kreslení želví grafiky.
+    {
+        double velikostStrany;
+        velikostStrany = 150;
+
+        double velikostStranyB;
+        velikostStranyB = 200;
     }
 
+    public void start() {
+
+        nakresliRovnostrannyTrojuhelnik(150);
+        nakresliCtverec(100);
+        nakresliObdelnik(100, 150);
+        nakresliKolecko(30);
+
+    }
+
+    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany) {
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(120);
+    }
+
+    public void nakresliCtverec(double velikostStrany) {
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+
+    }
+
+    public void nakresliObdelnik(double velikostStrany, double velikostStranyB) {
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+        zofka.move(velikostStranyB);
+        zofka.turnLeft(90);
+        zofka.move(velikostStrany);
+        zofka.turnLeft(90);
+        zofka.move(velikostStranyB);
+        zofka.turnLeft(90);
+    }
+
+    public void nakresliKolecko(double velikostrStrany) {
+        for (int i = 0; i < 18; i++) {
+            zofka.move(velikostrStrany);
+            zofka.turnLeft(20);
+
+
+        }
+    }
 }
